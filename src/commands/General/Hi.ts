58d98d61ace45a5e7e2a9681fd4b6b58e7f9bc8b
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2022-01-26 09:08:12 -0800

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://media.tenor.com/videos/8a6408675b1f806290aaac4b939c5a98/mp4";
+			"https://c.tenor.com/CFowpSoO7VsAAAPo/adad.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `I am the eye of the wicked lord, member number zero, Rikka Takanashi!. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
