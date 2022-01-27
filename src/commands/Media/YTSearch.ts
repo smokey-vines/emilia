feab8661318b27311be44e05173b9ad80d60e335
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2022-01-27 02:54:03 -0800

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -36,7 +36,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `💖EMILIA💖`,
+                        body: `💃LUMINE💃`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
