be447a904d91a81a87da347a0a972126b55d5821
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2022-01-29 02:03:56 -0800

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -1,9 +1,7 @@
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
-
+import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
   constructor(client: WAClient, handler: MessageHandler) {
@@ -19,23 +17,6 @@ export default class Command extends BaseCommand {
   run = async (M: ISimplifiedMessage): Promise<void> => {
     const user = M.sender.jid;
     const result = await (await this.client.getUser(user)).wallet;
-    
-const buttons = [
-  {
-    buttonId: "wallet",
-    buttonText: { displayText: `${this.client.config.prefix}bank` },
-    type: 1,
-  },
-];
-
-
-const buttonMessage: any = {
-  contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-  footerText: "🎇 Beyond 🎇",
-  buttons: buttons,
-  headerType: 1,
-};
-await M.reply(buttonMessage, MessageType.buttonsMessage);
-
-  }
+    await M.reply(`👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`);
+  };
 }
