1a9d1749c23a6562223e6b0aa3e4f823cea3746b
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2022-02-06 17:35:17 -0800

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,12 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '_*🎆BEYOND OWNERS🎆*_\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n🎉 *Username: ${
-                    user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                (text += `#${index + 1}\n🎉 *Username*: @${user.split("@")[0]}`)
         )
-        text += `\n 💖LUMINE💖`
+        text += `\n 💖EMILIA💖`
         return void M.reply(text)
     }
 }
