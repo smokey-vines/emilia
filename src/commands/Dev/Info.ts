d441302c81e2f7baea847cd5800bf34949bc174c
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-02-27 18:30:34 +0200

    Update Info.ts

--- src/commands/Dev/Info.ts
+++ src/commands/Dev/Info.ts
@@ -35,7 +35,7 @@ export default class Command extends BaseCommand {
 		};
 		const uptime = () => formatTime(process.uptime());
 		await M.reply(
-			`*━━━❰ 💃LUMINE💃 ❱━━━*\n\n 🔮 *Groups: ${
+			`*━━━❰ Boyka ❱━━━*\n\n 🔮 *Groups: ${
 				chats.length
 			}*/69\n\n🟩 *Users: ${users}*\n\n🚦 *Uptime: ${uptime()}*`
 		);
